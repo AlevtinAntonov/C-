16b2dea4e6373d18b1d054e405ab4bceeedd6951
Alevtin <alevtin_antonov@mail.ru>
2022-08-23 22:13:11 +0300

    Задача 8

--- Program.cs
+++ Program.cs
@@ -58,3 +58,24 @@ if (num % 2 == 0)
 else{
    Console.WriteLine($"Число {num} не четное"); 
 }
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+int n = new Random().Next(1,20);
+int index = 0;
+Console.WriteLine($"Все чётные числа от 1 до {n} :");
+while (index < n )
+{
+    if (index % 2 == 0 )
+    {
+        Console.WriteLine(index);
+    }
+    index = index + 2;
+}
+
+
+
